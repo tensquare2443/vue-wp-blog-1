13129cd88e5d219bbb3b7fb7a7e43f1a951d3c06
tensquare2443 <owner@Kevins-MacBook-Pro.local>
2019-11-30 14:52:38 -0500

    .

--- src/functions/fetch-data/getKey.js
+++ src/functions/fetch-data/getKey.js
@@ -1,9 +1,11 @@
 // if (process.env.key) {
-  var apiKey = process.env.API_KEY;
+  // var apiKey = process.env.API_KEY;
 // } else {
   // key = require("../../vars").key;
 // }
  
 // var apiKey = require("../../vars").key;
+
+var apiKey = "PtbITpmh1o9SPBHNcoNP2JPKhUujTGr6BNrx6Uv3";
 
 module.exports = { apiKey };
