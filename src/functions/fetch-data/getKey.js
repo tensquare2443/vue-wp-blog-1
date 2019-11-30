8115e35bf9c9a973523171abbabaf6436fa7534c
tensquare2443 <owner@Kevins-MacBook-Pro.local>
2019-11-30 13:50:23 -0500

    ...

--- src/functions/fetch-data/getKey.js
+++ src/functions/fetch-data/getKey.js
@@ -1,7 +1,7 @@
-if (process.env.key) {
+// if (process.env.key) {
   var key = process.env.key;
-} else {
-  key = require("../../vars").key;
-}
+// } else {
+//   key = require("../../vars").key;
+// }
 
 module.exports = { key };
